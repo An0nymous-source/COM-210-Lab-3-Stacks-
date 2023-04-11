a60074f0864fdd20dcb6a81aac16926fc1c57989
An0nymous-source <78245267+An0nymous-source@users.noreply.github.com>
2023-04-11 14:55:05 -0400

    Add files via upload

---
+++ TestMyStack.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TestMyStack {
+  public static void main (String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+    
+    char[] input = sc.next().charAt(1);
+
+
+  }
+
+
+  public boolean push(){
+
+  }
+
+  public void pop(){
+    
+  }
+}
